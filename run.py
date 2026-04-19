938fa309d760eb348d89637ff51fd2a31fb284f2
Raphael Beauregard <rnga3672@gmail.com>
2026-04-19 15:51:52 -0400

    added tls

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import os
 
 if __name__ == "__main__":
    our_app = app.create_app()
-   our_app.run(host="127.0.0.1")
+   our_app.run(ssl_context=('selfcerts/cert.pem', 'selfcerts/key.pem'), host="127.0.0.1")
