b330aae440a969959e5c90f5ef267a8f9dce02b2
Raphael Beauregard <rnga3672@gmail.com>
2026-04-20 22:24:56 -0400

    added a timeout loop

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ./pyenv/bin/pip install -r ./requirements.txt 
-./pyenv/bin/python ./run.py
+while true; do
+    timeout 7200 ./pyenv/bin/python ./run.py
+    echo "Restarting after 2 hrs to clear threads."
+    sleep 2
+done
