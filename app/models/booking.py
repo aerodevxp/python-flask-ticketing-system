e4c88e80cfa43f55fabea5a2f3d1dbd73a9c3d90
Raphael Beauregard <rnga3672@gmail.com>
2026-04-21 08:27:36 -0400

    .

--- app/models/booking.py
+++ app/models/booking.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class Booking(db.Model):
     __tablename__ = 'bookings'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id', ondelete='CASCADE'), nullable=False)  # ← AJOUTE ondelete='CASCADE'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id', ondelete='CASCADE'), nullable=True)  # ← AJOUTE ondelete='CASCADE'
     statut = db.Column(db.String(20), default='confirmé')
     date_reservation = db.Column(db.DateTime, default=datetime.utcnow)
     
--- app/models/user.py
+++ app/models/user.py
@@ -4,9 +4,9 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(100), nullable=False)
+    nom = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    mot_de_passe = db.Column(db.String(255), nullable=False)
+    mot_de_passe = db.Column(db.String(255), nullable=True)
     role = db.Column(db.String(20), default='user')
     
     def __repr__(self): #what to return if only the user object is asked for / example: print(user) would be <User EMAIL>
